207a7d7aaaf303b2cfcbb4791681d6add4b0c45e
Anna Gabain <anna.gabain@outlook.com>
2022-06-17 10:30:19 +0000

    Create game functions strucutre

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,39 @@
 // console.log("Connected!")
+
+function runGame(){
+
+}
+
+function checkAnswer(){
+    
+}
+
+// this is a helper function
+function calculateAnswer(){
+    
+}
+
+function incrementScore(){
+    
+}
+
+// if the user gets it wrong
+function incrementWrongAnswer(){
+    
+}
+
+function displayAdditionQuestion(){
+    
+}
+
+function displaySubtractQuestion(){
+    
+}
+
+function displayMultiplyQuestion(){
+    
+}
+
+// function displayDivideQuestion(){
+    
+// }
