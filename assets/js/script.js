2f793473dbdcc90829e64a2be8290018ce246ec4
Anna Gabain <anna.gabain@outlook.com>
2022-06-18 10:30:18 +0000

    Display the addition question

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,11 +20,14 @@ document.addEventListener("DOMContentLoaded", function(){
             }
             else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You have clicked ${gameType}`)
+                // alert(`You have clicked ${gameType}`) replaced this alert with the code below
+                runGame(gameType);
             }
 
         })
     }
+
+    runGame("addition");
 
 })
 
@@ -32,11 +35,18 @@ document.addEventListener("DOMContentLoaded", function(){
  * The main game 'loop', called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(){
+function runGame(gameType){
 
     // Created two random numbers betwwen 1 and 25
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2)
+    } else {
+        alert(`Unknown game type ${gameType}`)
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -59,8 +69,10 @@ function incrementWrongAnswer(){
     
 }
 
-function displayAdditionQuestion(){
-    
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion(){
