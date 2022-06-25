aeb5f7dce08ddfcabbb5a957753806725ffa2afc
Anna Gabain <anna.gabain@outlook.com>
2022-06-25 18:50:01 +0000

    Start division game challenge by adding the main functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,7 +61,10 @@ function runGame(gameType) {
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2)
 
-    } else {
+    } else if (gameType === "division") {
+        displayDivideQuestion(num1, num2)
+
+    }else {
         alert(`Unknown game type ${gameType}`)
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -110,7 +113,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else {
+    } else if (operator === "÷") {
+        return [operand1 / operand2, "division"];
+    }else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -154,6 +159,9 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 }
 
-// function displayDivideQuestion(){
-
-// }
+// CHALLENGE 
+function displayDivideQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand2 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "÷";
+}
