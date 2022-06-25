e717425aa2a891cea81ecf50f257396c419081e3
Anna Gabain <anna.gabain@outlook.com>
2022-06-25 17:31:21 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,8 +62,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right :D");
+        incrementScore();
     } else {
         alert(`Awwww... you answered ${userAnswer}. The correct Answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
 
     }
 
@@ -93,13 +95,23 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
 // if the user gets it wrong
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
